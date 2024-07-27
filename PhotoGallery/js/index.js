77a3626baebf4cf763bfe556e756348ddf09667a
Bruno Martins <jbrunno17@gmail.com>
2024-07-26 23:50:13 -0300

    Projeto PhotoGallery terminado

--- PhotoGallery/js/index.js
+++ PhotoGallery/js/index.js
@@ -1,21 +1,58 @@
+const button = document.getElementById('get-photos');
+const gallery = document.getElementById('gallery');
+const errorMessage = document.getElementById('errorMessage');
+const loadingSpiner = document.getElementById('loading-spinner');
+
+
+let randomPage = () =>  {
+    return Math.floor(Math.random() * 500);
+};
+
+let createNewPhoto = (photo) => {
+    let newPhoto = document.createElement("img");
+    newPhoto.src = photo.src.portrait;
+    newPhoto.alt = photo.alt;
+    newPhoto.classList.add("gallery-img")
+
+    return newPhoto;
+}
+
 async function fetchImages() {
-    let photos_amount = document.getElementById('photos-amount').value;
-    let gallery = document.getElementById('gallery');
-    await fetch(`https://api.pexels.com/v1/curated?&per_page=${photos_amount}`, {
+    let photoAmount = document.getElementById('photos-amount').value;
+
+    button.style.display = "none";
+    loadingSpiner.style.display = "block";
+
+    await fetch(`https://api.pexels.com/v1/curated?&per_page=${photoAmount}&page=${randomPage()}`, {
         headers: {
             Authorization: "QCHtV2WoeqgqAACLL6Get70uPT60nMBUdCqNDVsggioU5M8Hq2xXmPYK"
         }
     })
         .then(response => response.json())
         .then(data => data.photos.forEach(photo => {
-            let newPhoto = document.createElement("img");
-            newPhoto.src = photo.url;
-            newPhoto.alt = photo.name;
+            if (photoAmount < 1 || photoAmount > 10) {
+                button.style.display = "block";
 
-            gallery.appendChild(newPhoto)
+                loadingSpiner.style.display = "none";
+
+                errorMessage.innerText = "Enter a number between 1 and 10"
+                errorMessage.style.display = 'block';
+                return;
+            }
+
+            errorMessage.style.display = 'none';
+            gallery.prepend(createNewPhoto(photo));
+            button.style.display = "block";
+            loadingSpiner.style.display = "none";
+
+            gallery.hasChildNodes() ? gallery.style.display = 'flex' : gallery.style.display = 'none';
         }))
-        .catch(error => console.error('Error:', error));
+        .catch(err => {
+            button.style.display = "block";
+            loadingSpiner.style.display = "none";
+            errorMessage.innerText = "An error ocurred while fetching images";
+            errorMessage.style.display = 'block';
+        });
 }
 
-const button = document.getElementById('get-photos');
 button.addEventListener('click', fetchImages);
